a02f45e672c71c903072a847675f79eef29a941e
John Doe <olegnastyle@yandex.ru>
2024-08-28 21:18:42 +0300

    update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,34 +1,11 @@
 <template>
   <Header />
-  <Counter />
-  <Counter />
-  <Counter />
-  <Header />
 </template>
 
-
 <script setup>
-  import { onMounted, ref } from 'vue'
-  import Header from './components/Header.vue'
-  import Counter from './components/Counter.vue'
-
-  const counter = ref(0)
-  const message = ref('')
-  const checked = ref('true')
-  
-  // function increment () {
-  //   counter.value++
-  // }
-
-  // хуки жизненного цикла
-  onMounted(() => {
-    console.log(counter)
-  })
+  import Header from "./components/Header.vue";
 </script>
 
-<style>
-#app {
-  width: 900px;
-  margin: auto;
-}
+<style scoped>
+
 </style>
